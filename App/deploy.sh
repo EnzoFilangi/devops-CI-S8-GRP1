7f66f321c7203b0edb986a3db490d0f0779dba35
samuelbader <samuel.bader83@gmail.com>
2022-06-05 16:50:19 +0200

    update CD

--- App/deploy.sh
+++ App/deploy.sh
@@ -1,11 +1,11 @@
 echo "Deploying changes..."
 
-cd /home/devops/devops-CI-S8-GRP1/App
+cd /home/devops/devops-CI-S8-GRP1
 git pull
 
-docker-compose down
+docker-compose -f /App/docker-compose.yml down
 
-docker-compose build
+docker-compose /App/docker-compose.yml build
 
-docker-compose up -d
+docker-compose /App/docker-compose.yml up -d
 echo "Deployed!"
